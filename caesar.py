fa75ef4440834bf62fe9b178cb6dd195b4fbe93a
landongw <landon@extantpublishing.com>
2017-09-20 11:59:41 -0500

    Make pythonic

--- caesar.py
+++ caesar.py
@@ -1,9 +1,16 @@
+""" Runs the Caesar Cipher """
+
+
 def alphabet_position(character):
+    """ Determines the numeric position of the character """
+
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     lower = character.lower()
     return alphabet.index(lower)
 
+
 def rotate_string_13(text):
+    """ Rotates the string by 13 """
 
     rotated = ''
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
@@ -17,21 +24,25 @@ def rotate_string_13(text):
 
     return rotated
 
+
 def rotate_character(char, rot):
+    """ Rotates the character by a specified number """
+
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     rotated_idx = (alphabet_position(char) + rot) % 26
 
     if char.isupper():
         return alphabet[rotated_idx].upper()
-    else:
-        return alphabet[rotated_idx]
+    return alphabet[rotated_idx]
+
 
 def rotate_string(text, rot):
+    """ Rotates the string by a specified number """
 
     rotated = ''
 
     for char in text:
-        if (char.isalpha()):
+        if char.isalpha():
             rotated = rotated + rotate_character(char, rot)
         else:
             rotated = rotated + char
